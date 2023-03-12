ff90005882836d5455bc4a3d35f56c1dba48bfd3
Juan Diego S <juandiegoo_93@hotmail.com>
2023-03-12 05:18:51 -0500

    clase 01

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*{html,js}'],
   content: [],
   theme: {
+    colors: {
+      ...colors,
+    },
     extend: {},
   },
   plugins: [],
-}
+};
+
